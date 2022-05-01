0c7c19c846ac891775dc359fdf048afb9d67b366
MattMcDowall <mcdowallml@unk.edu>
2022-05-01 18:05:35 -0500

    Prepare fields to be filled & dataframe for matches

--- AddEnumChron.py
+++ AddEnumChron.py
@@ -11,4 +11,19 @@ queryUpdateItem = '/almaws/v1/bibs/{mms_id}/holdings/{holding_id}/items/{item_pi
 
 ### Get MMS/Holding/Item IDs, Descriptions, Locations from spreadsheet ###
 print("Reading Excel file . . .")
-df = pd.read_csv("FullItemList.csv", converters={'Item ID': str})
+df = pd.read_csv("FullItemList.csv", converters={'Item ID': str, 'Holdings ID': str, 'MMS ID': str})
+# Strip it down to just the necessary columns
+df = df[['MMS ID', 'Holdings ID', 'Item ID', 'Description', 'Permanent Location']]
+# Remove leading/trailing space from Description
+df.Description = df.Description.str.strip()
+# Collapse multiple spaces within the Description
+df.Description.replace(' +', ' ', regex=True, inplace=True)
+
+###################################################################
+### Split the Description out to appropriate Enum/Chron columns ###
+###################################################################
+print("Doing the replacements . . .")
+# Add the fields to be filled
+df[['EnumA', 'EnumB', 'ChronI', 'ChronJ']] = None
+# Create a dataframe to hold JUST records that get filled
+filled = pd.DataFrame()
