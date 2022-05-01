a98f35be2d37a13d0ca84b4b040ff890e598a963
MattMcDowall <mcdowallml@unk.edu>
2022-05-01 17:47:56 -0500

    Add Jupyter scratchbook & fix CSV encoding

--- AddEnumChron.py
+++ AddEnumChron.py
@@ -1,5 +1,14 @@
-# from credentials import *   # get API keys etc.
-import Credentials
+import Credentials      # Get API keys, etc.
+import pandas as pd
+from pprint import pprint as pp
+
+pp()    # So linter thinks it's being used
 
 apikey = Credentials.apikey
-print(apikey)
+
+baseurl = 'https://api-na.hosted.exlibrisgroup.com'
+queryUpdateItem = '/almaws/v1/bibs/{mms_id}/holdings/{holding_id}/items/{item_pid}?apikey={apikey}'
+
+### Get MMS/Holding/Item IDs, Descriptions, Locations from spreadsheet ###
+print("Reading Excel file . . .")
+df = pd.read_csv("FullItemList.csv", converters={'Item ID': str})
