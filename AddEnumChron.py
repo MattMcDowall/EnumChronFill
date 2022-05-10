29440f411641f57bff5a883d1cea57f5ace140ed
Matt McDowall <mcdowallml@unk.edu>
2022-05-10 09:23:24 -0500

    Clear up ambiguous use of "fields" var name

--- AddEnumChron.py
+++ AddEnumChron.py
@@ -12,9 +12,9 @@ queryUpdateItem = '/almaws/v1/bibs/{mms_id}/holdings/{holding_id}/items/{item_pi
 ### Get MMS/Holding/Item IDs, Descriptions, Locations from spreadsheet ###
 print("Reading Excel file . . .")
 df = pd.read_csv("FullItemList.csv", converters={'Item ID': str, 'Holdings ID': str, 'MMS ID': str})
-# Strip it down to just the necessary columns
+# Pare it down to just the necessary columns
 df = df[['MMS ID', 'Holdings ID', 'Item ID', 'Description', 'Permanent Location']]
-# Remove leading/trailing space from Description
+# Strip leading/trailing space from Description
 df.Description = df.Description.str.strip()
 # Collapse multiple spaces within the Description
 df.Description.replace(' +', ' ', regex=True, inplace=True)
@@ -25,14 +25,14 @@ df.columns = [c.replace(' ', '') for c in df.columns]
 ### Split the Description out to appropriate Enum/Chron columns ###
 ###################################################################
 print("Doing the replacements . . .")
-# Add the fields to be filled
-fields = ['EnumA', 'EnumB', 'ChronI', 'ChronJ']
-df[fields] = None
+# Add columns for the Enum/Chron fields
+ECfields = ['EnumA', 'EnumB', 'ChronI', 'ChronJ']
+df[ECfields] = None
 
 
-def fillAndExtract(regex, fields):
+def fillAndExtract(regex, theseFields):
     exp = re.compile(regex)
-    for i, f in enumerate(fields):
+    for i, f in enumerate(theseFields):
         df[f] = df['Description'].str.extract(exp, expand=True)[i]
 
 
@@ -44,6 +44,6 @@ fillAndExtract(r'^(v)\.(\d)+$', ['EnumA', 'EnumB'])
 # Create a dataframe to hold JUST records that get filled
 filled = pd.DataFrame()
 # Populate the new dataframe with any records that now have Enum/Chron info
-filled = df.dropna(subset=fields, thresh=1)
+filled = df.dropna(subset=ECfields, thresh=1)
 # Purge records from the original dataframe if they are now in the new one
 df = df.loc[~df['ItemID'].isin(filled['ItemID'])]
